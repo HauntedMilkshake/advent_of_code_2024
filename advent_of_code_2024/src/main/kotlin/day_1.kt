c6ed349c79b574b8fb770de6c88f144f86fffc84
HauntedMilkshake <spaszahov@gmail.com>
2024-12-02 21:53:21 +0200

    semi solution for 1

--- advent_of_code_2024/src/main/kotlin/day_1.kt
+++ advent_of_code_2024/src/main/kotlin/day_1.kt
@@ -8,22 +8,43 @@ fun calculateTotalDistance(firstList: MutableList<Int>, secondList: MutableList<
     }
     return sum;
 }
+
+fun calculateSimilarityScore(map: MutableMap<Int, Int>, firstList: List<Int>): Int {
+   var sum = 0
+    firstList.toSet().forEach {
+        if(map.containsKey(it)) {
+            sum += it * (map[it] ?: 0)
+        }
+    }
+    return sum
+}
 fun main() {
     val input = File("src\\main\\kotlin\\input_1.txt").bufferedReader().readLines()
     val firstList = mutableListOf<Int>()
     val secondList = mutableListOf<Int>()
+    //key - number from first list
+    //value - times of occurrences
+    val map = mutableMapOf<Int, Int>()
 
     var subList: List<String>
 
     input.forEachIndexed { index, string ->
         subList = string.split("   ")
         firstList.add(index, subList[0].toInt())
+
+        if(map.containsKey(subList[1].toInt())) {
+            map[subList[1].toInt()] = (map[subList[1].toInt()] ?: 0) * subList[1].toInt()
+        } else {
+            map[subList[1].toInt()] = 1
+        }
+
         secondList.add(index, subList[1].toInt())
 
     }
-
+    println(map)
     firstList.sort()
     secondList.sort()
 
     println(calculateTotalDistance(firstList, secondList))
+    println(calculateSimilarityScore(map, firstList))
 }
